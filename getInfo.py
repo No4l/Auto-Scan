8a1f500776b689f01eae6dcfdf78dac9264c650b
No4l <yubanbanz@gmail.com>
2018-11-30 14:12:21 +0800

    Get Info

--- getInfo.py
+++ getInfo.py
@@ -1,21 +1,74 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from queue import Queue
+import threading
 import requests
 import sys
 
-def load_file(fileName,nums):
-    target = set()
-    with open(fileName,'r') as f:
-        count = 0
-        for i in f.readlines():
-            target.add(i.strip('\n'))
-            count += 1
-            if count == nums:
-                break
-    return target
 
-def get_info(target):
-    
+q = Queue()
+p = Queue()
+'''
+从目标文件中读取指定条数的链接
+'''
+def load_file(fileName,start,nums):
+    #target = set()
+    with open(fileName,'r') as f:
+        a = f.readlines()
+        for i in range(start,nums):
+            #target.add(i.strip('\n'))
+            q.put(a[i].strip('\n'))
+    #return target
+'''
+从目标链接中选择PHP
+'''
+def get_php(target):
+    try:
+        res = requests.get(target,timeout=1)
+        origin = len(res.text)
+    except:
+        return
+    if 'X-Powered-By' in res.headers:
+        if 'PHP' in res.headers['X-Powered-By']:
+            print('[PHP]PHP Find... '+target)
+            if 'ThinkPHP' in res.headers['X-Powered-By']:
+                print('[PHP]ThinkPHP Find... '+target)
+            return 1
+    else:
+            try:
+                res = requests.get(target+'/index.php',timeout=1,allow_redirects=False)
+                now = len(res.text)
+            except:
+                return
+            if res.status_code == 200 and (now+100)>=origin:
+                print('[PHP]PHP Find... '+target)
+                return 1
+'''
+开始扫描
+'''
+def scan_thread():
+    while not q.empty():
+        target = q.get()
+        if get_php(target):
+            info_scan(target)
+
+'''
+get infosec
+'''
+def info_scan(target):
+    try:
+        res = requests.get(target+'/robots.txt',timeout=1)
+    except:
+        return
+    if res.status_code == 200 and 'User-agent' in res.text:
+        print('[INFO]robots find...,'+target+'robots.txt')
+
+
+
+
 
 if __name__ == '__main__':
-    target = load_file('butian.txt',10)
+    target = load_file('butian.txt',100,200)
+    for i in range(10):
+        t1 = threading.Thread(target=scan_thread)
+        t1.start()
